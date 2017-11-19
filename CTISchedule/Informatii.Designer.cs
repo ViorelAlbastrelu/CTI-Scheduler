9aaacdbce0f1a70b5fef6f1cdb553edc7f0dc89c
valbastrelu <viorel.albastrelu@footasylum.com>
2017-11-19 21:56:07 +0200

    added "Informatii" window form

---
+++ CTISchedule/Informatii.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace CTISchedule
+{
+    partial class Informatii
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabInformatii = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabInformatii.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabInformatii
+            // 
+            this.tabInformatii.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabInformatii.Controls.Add(this.tabPage1);
+            this.tabInformatii.Controls.Add(this.tabPage2);
+            this.tabInformatii.Controls.Add(this.tabPage3);
+            this.tabInformatii.Controls.Add(this.tabPage4);
+            this.tabInformatii.Controls.Add(this.tabPage5);
+            this.tabInformatii.Controls.Add(this.tabPage6);
+            this.tabInformatii.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabInformatii.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.tabInformatii.ItemSize = new System.Drawing.Size(30, 120);
+            this.tabInformatii.Location = new System.Drawing.Point(0, 0);
+            this.tabInformatii.Multiline = true;
+            this.tabInformatii.Name = "tabInformatii";
+            this.tabInformatii.SelectedIndex = 0;
+            this.tabInformatii.Size = new System.Drawing.Size(484, 261);
+            this.tabInformatii.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabInformatii.TabIndex = 0;
+            this.tabInformatii.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.tabInformatii_DrawItem);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(124, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(356, 253);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Intro";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(124, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(356, 253);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Orar";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(124, 4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(356, 253);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Modul";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(124, 4);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(356, 253);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "CRUD";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(124, 4);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(356, 253);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Import";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Location = new System.Drawing.Point(124, 4);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(356, 253);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Export";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // Informatii
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.tabInformatii);
+            this.Name = "Informatii";
+            this.Text = "Informatii";
+            this.tabInformatii.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabInformatii;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+    }
+}
