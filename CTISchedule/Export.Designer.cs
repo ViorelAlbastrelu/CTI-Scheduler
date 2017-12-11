a0680c19d8d63989c5b9edc201e5873c079fdfcf
valbastrelu <viorel.albastrelu@footasylum.com>
2017-12-12 00:16:32 +0200

    Minor visual adjusments to Export.cs

--- CTISchedule/Export.Designer.cs
+++ CTISchedule/Export.Designer.cs
@@ -29,31 +29,73 @@
         private void InitializeComponent()
         {
             this.exportPanel = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.activeLabel = new System.Windows.Forms.Label();
             this.exportButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
             this.activeComboBox = new System.Windows.Forms.ComboBox();
-            this.activeLabel = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.exportPanel.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // exportPanel
             // 
-            this.exportPanel.Controls.Add(this.exportButton);
-            this.exportPanel.Controls.Add(this.cancelButton);
+            this.exportPanel.Controls.Add(this.tableLayoutPanel2);
+            this.exportPanel.Controls.Add(this.tableLayoutPanel1);
             this.exportPanel.Controls.Add(this.activeComboBox);
-            this.exportPanel.Controls.Add(this.activeLabel);
-            this.exportPanel.Controls.Add(this.label1);
             this.exportPanel.Location = new System.Drawing.Point(12, 12);
             this.exportPanel.Name = "exportPanel";
-            this.exportPanel.Size = new System.Drawing.Size(372, 271);
+            this.exportPanel.Size = new System.Drawing.Size(317, 159);
             this.exportPanel.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.activeLabel, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(20, 20);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(275, 41);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Alegeti";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // activeLabel
+            // 
+            this.activeLabel.AutoSize = true;
+            this.activeLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.activeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.activeLabel.Location = new System.Drawing.Point(106, 0);
+            this.activeLabel.Name = "activeLabel";
+            this.activeLabel.Size = new System.Drawing.Size(166, 41);
+            this.activeLabel.TabIndex = 1;
+            this.activeLabel.Text = "Profesorul";
+            this.activeLabel.Click += new System.EventHandler(this.label2_Click);
             // 
             // exportButton
             // 
-            this.exportButton.Location = new System.Drawing.Point(198, 204);
+            this.exportButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.exportButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exportButton.Location = new System.Drawing.Point(141, 3);
             this.exportButton.Name = "exportButton";
-            this.exportButton.Size = new System.Drawing.Size(126, 23);
+            this.exportButton.Size = new System.Drawing.Size(132, 29);
             this.exportButton.TabIndex = 4;
             this.exportButton.Text = "Exporta";
             this.exportButton.UseVisualStyleBackColor = true;
@@ -61,9 +103,11 @@
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(49, 204);
+            this.cancelButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelButton.Location = new System.Drawing.Point(3, 3);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(126, 23);
+            this.cancelButton.Size = new System.Drawing.Size(132, 29);
             this.cancelButton.TabIndex = 3;
             this.cancelButton.Text = "Anuleaza";
             this.cancelButton.UseVisualStyleBackColor = true;
@@ -75,44 +119,38 @@
             this.activeComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.activeComboBox.FormattingEnabled = true;
             this.activeComboBox.IntegralHeight = false;
-            this.activeComboBox.Location = new System.Drawing.Point(48, 125);
+            this.activeComboBox.Location = new System.Drawing.Point(20, 76);
             this.activeComboBox.Name = "activeComboBox";
-            this.activeComboBox.Size = new System.Drawing.Size(276, 28);
+            this.activeComboBox.Size = new System.Drawing.Size(275, 28);
             this.activeComboBox.TabIndex = 2;
             // 
-            // activeLabel
+            // tableLayoutPanel2
             // 
-            this.activeLabel.AutoSize = true;
-            this.activeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.activeLabel.Location = new System.Drawing.Point(161, 52);
-            this.activeLabel.Name = "activeLabel";
-            this.activeLabel.Size = new System.Drawing.Size(163, 37);
-            this.activeLabel.TabIndex = 1;
-            this.activeLabel.Text = "Profesorul";
-            this.activeLabel.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(41, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 37);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Alegeti";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.cancelButton, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.exportButton, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(20, 110);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(276, 35);
+            this.tableLayoutPanel2.TabIndex = 6;
             // 
             // Export
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 295);
+            this.ClientSize = new System.Drawing.Size(341, 181);
             this.Controls.Add(this.exportPanel);
             this.Name = "Export";
             this.Text = "Export";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.exportFormClosed);
             this.exportPanel.ResumeLayout(false);
-            this.exportPanel.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -125,5 +163,7 @@
         private System.Windows.Forms.ComboBox activeComboBox;
         private System.Windows.Forms.Button exportButton;
         private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
     }
 }
